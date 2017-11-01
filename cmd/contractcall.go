4ec9fe215a9145727ceca34a057478fe29e6be3c
Jim McDonald <Jim@mcdee.net>
2017-11-01 09:12:20 +0000

    Support more return types

--- cmd/contractcall.go
+++ cmd/contractcall.go
@@ -15,12 +15,15 @@ package cmd
 
 import (
 	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"math/big"
 	"os"
+	"strconv"
 	"strings"
 
 	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto/sha3"
 	"github.com/orinocopay/go-etherutils/cli"
 	"github.com/orinocopay/go-etherutils/ens"
@@ -69,6 +72,7 @@ In quiet mode this will return 0 if the contract is successfully called, otherwi
 		defer cancel()
 		result, err := client.CallContract(ctx, msg, nil)
 		cli.ErrCheck(err, quiet, fmt.Sprintf("Failed to call contract %s", contractCallSignature))
+		cli.Assert(len(result) > 0, quiet, fmt.Sprintf("Call to %s did not return any data", contractCallSignature))
 
 		if quiet {
 			os.Exit(0)
@@ -77,25 +81,68 @@ In quiet mode this will return 0 if the contract is successfully called, otherwi
 		// Format the result
 		cli.Assert(contractCallReturns != "", quiet, "--returns is required")
 		returnTypes := strings.Split(contractCallReturns, ",")
+		results := []string{}
 		position := 0
 		for _, returnType := range returnTypes {
-			switch returnType {
-			case "uint":
-			case "uint256":
+			cli.Assert(position < len(result), quiet, "Not enough data returned for all expected return values")
+			if strings.HasPrefix(returnType, "uint") {
+				var bytes int
+				if returnType == "uint" {
+					// Alias for uint256
+					bytes = 32
+				} else {
+					bytes, err = strconv.Atoi(returnType[4:])
+					cli.ErrCheck(err, quiet, fmt.Sprintf("Unknown return type %s", returnType))
+					bytes /= 8
+				}
 				item := big.NewInt(0)
-				item.SetBytes(result[position : position+32])
+				item.SetBytes(result[position+32-bytes : position+32])
 				position += 32
-				fmt.Println(item.String())
-			case "string":
+				results = append(results, item.String())
+			} else if strings.HasPrefix(returnType, "int") {
+				// TODO how to handle negative numbers
+				var bytes int
+				if returnType == "int" {
+					// Alias for int256
+					bytes = 32
+				} else {
+					bytes, err = strconv.Atoi(returnType[4:])
+					cli.ErrCheck(err, quiet, fmt.Sprintf("Unknown return type %s", returnType))
+					bytes /= 8
+				}
+				item := big.NewInt(0)
+				item.SetBytes(result[position+32-bytes : position+32])
+				position += 32
+				results = append(results, item.String())
+			} else if returnType == "string" {
 				offset := binary.BigEndian.Uint64(result[position+24 : position+32])
 				length := binary.BigEndian.Uint64(result[offset+24 : offset+32])
 				item := string(result[offset+32 : offset+32+length])
 				position += 32
-				fmt.Println(item)
-			default:
-				cli.Err(quiet, fmt.Sprintf("Unknown return type %s", returnType))
+				results = append(results, fmt.Sprintf("\"%s\"", item))
+			} else if returnType == "address" {
+				item := common.BytesToAddress(result[position+12 : position+32])
+				position += 32
+				results = append(results, item.Hex())
+			} else if returnType == "hash" {
+				item := result[position : position+32]
+				position += 32
+				results = append(results, fmt.Sprintf("0x%s", hex.EncodeToString(item)))
+			} else if returnType == "bool" {
+				item := result[position+31]
+				position += 32
+				if item == 1 {
+					results = append(results, "true")
+				} else {
+					results = append(results, "false")
+				}
+			} else {
+				cli.Err(quiet, fmt.Sprintf("Unknown return type %s\nData is %s", returnType, hex.EncodeToString(result)))
+				results = append(results, "?")
 			}
+			// TODO fixed, ufixed, bytes, function, <type>[M], bytes, <type>[], ()
 		}
+		fmt.Printf("%s\n", strings.Join(results, ","))
 	},
 }
 
