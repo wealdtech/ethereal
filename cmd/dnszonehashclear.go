5769c6f75c082165813f3358c1793c7e9c1e06c3
Jim McDonald <Jim@mcdee.net>
2020-01-03 09:40:19 +0000

    Add DNS zonehash clear

--- cmd/dnszonehashclear.go
+++ cmd/dnszonehashclear.go
@@ -24,16 +24,14 @@ import (
 	ens "github.com/wealdtech/go-ens/v3"
 )
 
-var dnsZonehashSetZonehashStr string
-
-// dnsZonehashSetCmd represents the dns zonehash set command
-var dnsZonehashSetCmd = &cobra.Command{
-	Use:   "set",
-	Short: "Set the zone hash of a DNS domain held in ENS",
-	Long: `Set the zone hash of a DNS domain registered with the Ethereum Name Service (ENS).  For example:
+// dnsZonehashClearCmd represents the dns zonehash clear command
+var dnsZonehashClearCmd = &cobra.Command{
+	Use:   "clear",
+	Short: "Clear the zone hash of a DNS domain held in ENS",
+	Long: `Clear the zone hash of a DNS domain registered with the Ethereum Name Service (ENS).  For example:
 
  TODO
-    ethereal dns zone set --domain=enstest.eth --zonehash=/swarm/d1de9994b4d039f6548d191eb26786769f580809256b4685ef316805265ea162 --passphrase="my secret passphrase"
+    ethereal dns zone clear --domain=enstest.eth --passphrase="my secret passphrase"
 
 The keystore for the account that owns the name must be local (i.e. listed with 'get accounts list') and unlockable with the supplied passphrase.
 
@@ -54,11 +52,6 @@ This will return an exit status of 0 if the transaction is successfully submitte
 		cli.ErrCheck(err, quiet, "Failed to obtain name hash of ENS domain")
 		outputIf(verbose, fmt.Sprintf("ENS domain hash is 0x%x", domainHash))
 
-		cli.Assert(dnsZonehashSetZonehashStr != "", quiet, "--zonehash is required; if you are trying to clear an existing zone use \"dns zonehash clear\"")
-		data, err := ens.StringToContenthash(dnsZonehashSetZonehashStr)
-		cli.ErrCheck(err, quiet, "Invalid zone")
-		outputIf(verbose, fmt.Sprintf("Zonehash is %#x", data))
-
 		// Obtain the registry contract
 		registry, err := ens.NewRegistry(client)
 		cli.ErrCheck(err, quiet, "Cannot obtain ENS registry contract")
@@ -77,21 +70,19 @@ This will return an exit status of 0 if the transaction is successfully submitte
 		opts, err := generateTxOpts(domainOwner)
 		cli.ErrCheck(err, quiet, "failed to generate transaction options")
 
-		signedTx, err := resolver.SetZonehash(opts, data)
+		signedTx, err := resolver.SetZonehash(opts, nil)
 		cli.ErrCheck(err, quiet, "failed to send transaction")
 
 		handleSubmittedTransaction(signedTx, log.Fields{
 			"group":     "dns/zone",
-			"command":   "set",
+			"command":   "clear",
 			"ensdomain": ensDomain,
-			"zonehash":  dnsZonehashSetZonehashStr,
 		}, true)
 	},
 }
 
 func init() {
-	dnsZonehashCmd.AddCommand(dnsZonehashSetCmd)
-	dnsZonehashFlags(dnsZonehashSetCmd)
-	dnsZonehashSetCmd.Flags().StringVar(&dnsZonehashSetZonehashStr, "zonehash", "", "The path to the zonefile e.g. /ipfs/QmdTEBPdNxJFFsH1wRE3YeWHREWDiSex8xhgTnqknyxWgu")
-	addTransactionFlags(dnsZonehashSetCmd, "passphrase for the account that owns the domain")
+	dnsZonehashCmd.AddCommand(dnsZonehashClearCmd)
+	dnsZonehashFlags(dnsZonehashClearCmd)
+	addTransactionFlags(dnsZonehashClearCmd, "passphrase for the account that owns the domain")
 }
