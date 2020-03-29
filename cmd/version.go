67ee35223ece0245c9bd8b4be5a2bbc75e124954
Jim McDonald <Jim@mcdee.net>
2020-03-29 10:15:20 +0100

    Update to latest version of go-ens

--- cmd/version.go
+++ cmd/version.go
@@ -30,7 +30,7 @@ var versionCmd = &cobra.Command{
 
     ethereal version.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("2.3.2")
+		fmt.Println("2.3.3")
 		if viper.GetBool("verbose") {
 			buildInfo, ok := dbg.ReadBuildInfo()
 			if ok {
