da9660d173f55ad25afb14678fe725366a7fe1d8
Jim McDonald <Jim@mcdee.net>
2020-07-03 17:53:50 +0100

    Bump version

--- cmd/version.go
+++ cmd/version.go
@@ -30,7 +30,7 @@ var versionCmd = &cobra.Command{
 
     ethereal version.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("2.3.17")
+		fmt.Println("2.3.18")
 		if viper.GetBool("verbose") {
 			buildInfo, ok := dbg.ReadBuildInfo()
 			if ok {
