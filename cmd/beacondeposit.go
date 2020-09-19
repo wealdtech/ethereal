e711cd7f1c0311d41891cd70cc45fdbfd3b203c2
Alexander van der Meij <github@vandermeij.tech>
2020-09-19 11:57:08 +0200

    set medalla as default beacon deposit contract

--- cmd/beacondeposit.go
+++ cmd/beacondeposit.go
@@ -379,6 +379,6 @@ func init() {
 	beaconDepositCmd.Flags().BoolVar(&beaconDepositAllowExcessiveDeposit, "allow-excessive-deposit", false, "Allow sending more than 32 Ether in a single deposit (WARNING: only if you know what you are doing)")
 	beaconDepositCmd.Flags().BoolVar(&beaconDepositAllowDuplicateDeposit, "allow-duplicate-deposit", false, "Allow sending multiple deposits with the same validator public key (WARNING: only if you know what you are doing)")
 	beaconDepositCmd.Flags().StringVar(&beaconDepositContractAddress, "address", "", "The address to which to send the deposit (overrides network)")
-	beaconDepositCmd.Flags().StringVar(&beaconDepositEth2Network, "eth2network", "onyx", "The name of the network to send the deposit to (topaz/onyx/altona/medalla/spadina)")
+	beaconDepositCmd.Flags().StringVar(&beaconDepositEth2Network, "eth2network", "medalla", "The name of the network to send the deposit to (topaz/onyx/altona/medalla/spadina)")
 	addTransactionFlags(beaconDepositCmd, "passphrase for the account that owns the account")
 }
